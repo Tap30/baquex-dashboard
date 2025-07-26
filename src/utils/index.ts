a293ba95d98c5e6d968f765e8420440cf37d00aa
mimshins <mostafa.sh.coderino@gmail.com>
2025-07-26 11:19:14 +0330

    refactor(utils): expose handle-ref utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 export * from "./cn.ts";
 export * from "./decode-jwt.ts";
 export * from "./generate-tailwind-classes-with-breakpoints.ts";
+export * from "./handle-ref.ts";
 export * from "./use-controllable-prop.ts";
 export * from "./use-forked-refs.ts";
 export * from "./use-isomorphic-layout-effect.ts";
--- src/utils/use-forked-refs.ts
+++ src/utils/use-forked-refs.ts
@@ -1,11 +1,5 @@
 import { useCallback } from "react";
-
-const setRef = <T>(ref: React.Ref<T>, instance: T | null) => {
-  if (typeof ref === "function") ref(instance);
-  else if (ref && typeof ref === "object" && "current" in ref) {
-    ref.current = instance;
-  }
-};
+import { handleRef } from "./handle-ref.ts";
 
 export const useForkedRefs = <T>(
   ...refs: (React.Ref<T> | undefined)[]
@@ -15,7 +9,7 @@ export const useForkedRefs = <T>(
       refs.forEach(ref => {
         if (!ref) return;
 
-        setRef(ref, instance);
+        handleRef(ref, instance);
       });
     },
     [refs],
