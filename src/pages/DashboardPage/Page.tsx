b924cba04cfaf7fb853a9b1f0e4a6a57c4136524
mimshins <mostafa.sh.coderino@gmail.com>
2025-07-26 21:03:30 +0330

    feat(dashboard-page): update page elements

--- src/pages/DashboardPage/Page.tsx
+++ src/pages/DashboardPage/Page.tsx
@@ -1,14 +1,27 @@
+import { useBreadcrumb, type BreadcrumbItem } from "@/components";
+import { strings } from "@/static-content";
+import { useEffect } from "react";
 import { useLoaderData } from "react-router";
+import classes from "./styles.module.css";
 import { type PageLoaderData } from "./types.ts";
 
 export const DashboardPage: React.FC = () => {
   const data = useLoaderData<PageLoaderData>();
 
-  console.log({ data });
+  const { addCrumb, removeCrumb } = useBreadcrumb();
 
-  return (
-    <section>
-      <h1>Dashboard Page</h1>
-    </section>
-  );
+  useEffect(() => {
+    const crumb: BreadcrumbItem = {
+      title: strings.sidebar.dashboard,
+      href: "/",
+    };
+
+    addCrumb(crumb);
+
+    return () => {
+      removeCrumb(crumb);
+    };
+  }, [addCrumb, removeCrumb]);
+
+  return <section className={classes["root"]}></section>;
 };
