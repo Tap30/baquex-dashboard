c87abe37b219516659f54d1a7b94dace24975602
mimshins <mostafa.sh.coderino@gmail.com>
2025-07-27 09:55:45 +0330

    feat(panel-card): add panel-card block

---
+++ src/components/blocks/PanelCard/PanelCard.tsx
@@ -0,0 +1,51 @@
+import { Flex, Text } from "@/components";
+import type { WithBaseProps } from "@/types";
+import { cn } from "@/utils";
+import { useId } from "react";
+import classes from "./styles.module.css";
+
+export type PanelCardProps = WithBaseProps<{
+  title: string;
+  subtitle: string;
+  headingSlot?: React.ReactNode;
+}>;
+
+export const PanelCard: React.FC<PanelCardProps> = props => {
+  const { subtitle, title, headingSlot, className, children } = props;
+
+  const nodeId = useId();
+  const titleId = `TITLE_${nodeId}`;
+
+  return (
+    <div
+      role="region"
+      aria-labelledby={titleId}
+      className={cn(classes["root"], className)}
+    >
+      <header className={classes["heading"]}>
+        <Flex
+          direction="column"
+          gap="xs"
+        >
+          <Text
+            as="strong"
+            variant="h6"
+            id={titleId}
+          >
+            {title}
+          </Text>
+          <Text
+            as="strong"
+            variant="subheading1"
+            id={titleId}
+            color="tertiary"
+          >
+            {subtitle}
+          </Text>
+        </Flex>
+        {headingSlot}
+      </header>
+      {children}
+    </div>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
+export * from "./blocks/index.ts";
 export * from "./elements/index.ts";
 export * from "./layouts/index.ts";
