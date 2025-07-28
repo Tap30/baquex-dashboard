1c3a7074f3bc3baa1cc75f11fbdb3ee58e5f01c3
mimshins <mostafa.sh.coderino@gmail.com>
2025-07-28 15:06:36 +0330

    feat: add `ref` prop

--- src/components/elements/Alert/Alert.tsx
+++ src/components/elements/Alert/Alert.tsx
@@ -1,38 +1,42 @@
 import { Flex, Text } from "@/components";
+import type { WithRef } from "@/types";
 import { cn } from "@/utils";
 import classes from "./styles.module.css";
 
-export type AlertProps = {
-  /**
-   * The className applied to the component.
-   */
-  className?: string;
+export type AlertProps = WithRef<
+  {
+    /**
+     * The className applied to the component.
+     */
+    className?: string;
 
-  /**
-   * The title of the alert.
-   */
-  title: string;
+    /**
+     * The title of the alert.
+     */
+    title: string;
 
-  /**
-   * The description of the alert.
-   */
-  description: string;
+    /**
+     * The description of the alert.
+     */
+    description: string;
 
-  /**
-   * The icon used for the alert message.
-   */
-  icon?: React.ReactNode;
+    /**
+     * The icon used for the alert message.
+     */
+    icon?: React.ReactNode;
 
-  /**
-   * The color scheme of the alert.
-   *
-   * @default "neutral"
-   */
-  color?: "neutral" | "brand" | "positive" | "negative" | "warn" | "info";
-};
+    /**
+     * The color scheme of the alert.
+     *
+     * @default "neutral"
+     */
+    color?: "neutral" | "brand" | "positive" | "negative" | "warn" | "info";
+  },
+  "div"
+>;
 
 export const Alert: React.FC<AlertProps> = props => {
-  const { description, title, className, icon, color = "neutral" } = props;
+  const { description, title, className, icon, color = "neutral", ref } = props;
 
   const renderIcon = () => {
     if (!icon) return null;
@@ -49,6 +53,7 @@ export const Alert: React.FC<AlertProps> = props => {
 
   return (
     <div
+      ref={ref}
       role="alert"
       className={cn(classes["root"], classes[color], className)}
     >
--- src/components/elements/Avatar/Avatar.tsx
+++ src/components/elements/Avatar/Avatar.tsx
@@ -1,41 +1,46 @@
+import type { WithRef } from "@/types";
 import { cn } from "@/utils";
 import * as AvatarPrimitives from "@radix-ui/react-avatar";
 import classes from "./styles.module.css";
 
-export type AvatarProps = {
-  /**
-   * The className applied to the component.
-   */
-  className?: string;
+export type AvatarProps = WithRef<
+  {
+    /**
+     * The className applied to the component.
+     */
+    className?: string;
 
-  /**
-   * The image URL.
-   */
-  src: string;
+    /**
+     * The image URL.
+     */
+    src: string;
 
-  /**
-   * The size of the component.
-   *
-   * @default "md"
-   */
-  size?: "sm" | "md" | "lg";
+    /**
+     * The size of the component.
+     *
+     * @default "md"
+     */
+    size?: "sm" | "md" | "lg";
 
-  /**
-   * Provides fallback (alternate) text to display when the image is not loaded.
-   */
-  alt: string;
+    /**
+     * Provides fallback (alternate) text to display when the image is not loaded.
+     */
+    alt: string;
 
-  /**
-   * An element that renders when the image hasn't loaded.
-   */
-  fallback?: React.ReactNode;
-};
+    /**
+     * An element that renders when the image hasn't loaded.
+     */
+    fallback?: React.ReactNode;
+  },
+  "span"
+>;
 
 export const Avatar: React.FC<AvatarProps> = props => {
-  const { alt, src, className, fallback, size = "md" } = props;
+  const { alt, src, className, fallback, size = "md", ref } = props;
 
   return (
     <AvatarPrimitives.Root
+      ref={ref}
       className={cn(classes["root"], classes[size], className)}
     >
       <AvatarPrimitives.Image
