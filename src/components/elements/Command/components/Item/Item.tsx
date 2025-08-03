4b59fe9a7d8d32c6b2029f4416d0c2bec8f20511
mimshins <mostafa.sh.coderino@gmail.com>
2025-08-03 20:54:40 +0330

    refactor(command): update styles

--- src/components/elements/Command/components/Item/Item.tsx
+++ src/components/elements/Command/components/Item/Item.tsx
@@ -1,17 +1,36 @@
 import { cn } from "@/utils";
 import { Command as CommandPrimitive } from "cmdk";
+import { useCallback } from "react";
 import classes from "./styles.module.css";
 
 export type CommandItemProps = React.ComponentProps<
   typeof CommandPrimitive.Item
->;
+> & {
+  /**
+   */
+  explicitlySelected?: boolean;
+};
 
 export const CommandItem: React.FC<CommandItemProps> = props => {
-  const { className, ...otherProps } = props;
+  const { className, explicitlySelected = false, ...otherProps } = props;
+
+  const refCallback = useCallback(
+    (node: HTMLDivElement | null) => {
+      if (!node) return;
+
+      const originalHighlightedState =
+        node.getAttribute("data-selected") ?? "false";
+
+      node.setAttribute("data-highlighted", originalHighlightedState);
+      node.setAttribute("data-explicitly-selected", String(explicitlySelected));
+    },
+    [explicitlySelected],
+  );
 
   return (
     <CommandPrimitive.Item
       {...otherProps}
+      ref={refCallback}
       className={cn(classes["root"], className)}
     />
   );
