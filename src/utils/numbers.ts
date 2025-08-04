681ed9874e717165aa973758c7b824f790936c58
Amirhossein Alibakhshi <amir78729@gmail.com>
2025-08-04 12:28:14 +0330

    feat(utils/numbers): add normalize numbers function

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -6,7 +6,35 @@ export const formatNumber = (n: number): string => {
 
   const formatter = new Intl.NumberFormat(
     lang === Languages.FA ? "fa-IR" : "en-US",
+    { useGrouping: false },
   );
 
   return formatter.format(n);
+};
+
+export const normalizeNumbers = (input: string): number => {
+  if (typeof input !== "string") {
+    return NaN;
+  }
+
+  let normalizedString = input.trim();
+
+  const persianDigits = {
+    "۰": "0",
+    "۱": "1",
+    "۲": "2",
+    "۳": "3",
+    "۴": "4",
+    "۵": "5",
+    "۶": "6",
+    "۷": "7",
+    "۸": "8",
+    "۹": "9",
+  } as Record<string, string>;
+
+  normalizedString = input.replace(/[۰-۹]/g, char => persianDigits[char] ?? "");
+
+  const parsedNumber = Number(normalizedString);
+
+  return isNaN(parsedNumber) ? NaN : parsedNumber;
 };
