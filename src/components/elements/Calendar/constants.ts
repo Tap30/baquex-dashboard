c40cfecca822a21bda2340d4e4f3c6515c6dbf8d
Amirhossein Alibakhshi <amir78729@gmail.com>
2025-08-06 13:55:31 +0330

    fix(calendar): remove the lazy loading for different types of calendars (for better ux)

--- src/components/elements/Calendar/constants.ts
+++ src/components/elements/Calendar/constants.ts
@@ -1,7 +1,15 @@
 import { Languages } from "@/constants";
 import { strings } from "@/static-content";
 import { faIR as enFaIR, enUS, type Locale } from "date-fns/locale";
-import { enUS as faEnUS, faIR } from "react-day-picker/persian";
+import {
+  type DayPicker,
+  DayPicker as GregorianDayPicker,
+} from "react-day-picker";
+import {
+  enUS as faEnUS,
+  faIR,
+  DayPicker as JalaliDayPicker,
+} from "react-day-picker/persian";
 
 export const CalendarType = {
   GREGORIAN: "GREGORIAN",
@@ -15,4 +23,12 @@ export const CalendarLocale: Record<
   [CalendarType.JALALI]: strings.getLanguage() === Languages.EN ? faEnUS : faIR,
   [CalendarType.GREGORIAN]:
     strings.getLanguage() === Languages.EN ? enUS : enFaIR,
+} as const;
+
+export const CalendarInstance: Record<
+  (typeof CalendarType)[keyof typeof CalendarType],
+  typeof DayPicker
+> = {
+  [CalendarType.JALALI]: JalaliDayPicker as typeof DayPicker,
+  [CalendarType.GREGORIAN]: GregorianDayPicker,
 } as const;
--- src/components/elements/Calendar/utils.ts
+++ src/components/elements/Calendar/utils.ts
@@ -1,18 +1,14 @@
-import { lazy } from "react";
-import { CalendarLocale, CalendarType } from "./constants.ts";
+import {
+  CalendarInstance,
+  CalendarLocale,
+  type CalendarType,
+} from "./constants.ts";
 
 export const resolveCalendar = (
   type: (typeof CalendarType)[keyof typeof CalendarType],
 ) => {
-  const DayPicker = lazy(async () => {
-    if (type === CalendarType.GREGORIAN) {
-      return { default: (await import("react-day-picker")).DayPicker };
-    }
-
-    return { default: (await import("react-day-picker/persian")).DayPicker };
-  });
-
   const locale = CalendarLocale[type];
+  const DayPicker = CalendarInstance[type];
 
   return { DayPicker, locale };
 };
