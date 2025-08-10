69d5e01c30b99e07a581b8ab0136a1841c7ea77e
mimshins <mostafa.sh.coderino@gmail.com>
2025-08-10 14:10:25 +0330

    refactor(auth): update auth layout

--- src/components/layouts/AuthLayout/AuthLayout.tsx
+++ src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -1,27 +1,38 @@
-import { DASHBOARD_PATH } from "@/constants";
+import { DASHBOARD_PATH, UNAUTHORIZED_PATH } from "@/constants";
 import { useAuth } from "@/contexts";
 import { Navigate, Outlet } from "react-router";
 
 export const AuthLayout: React.FC = () => {
-  const { isAuthenticated, isAuthenticating } = useAuth();
+  const { isAuthenticated, isAuthenticating, isAccessGranted } = useAuth();
 
   if (isAuthenticating) {
     return <div>Authenticating...</div>;
   }
 
-  // If authenticated, redirect to the dashboard page
-  if (isAuthenticated) {
+  // If not authenticated, render the login page
+  if (!isAuthenticated) {
+    return (
+      <main>
+        <Outlet />
+      </main>
+    );
+  }
+
+  // If authenticated but scope access is not granted, redirect to an unauthorized page
+  if (!isAccessGranted) {
     return (
       <Navigate
-        to={DASHBOARD_PATH}
+        to={UNAUTHORIZED_PATH}
         replace
       />
     );
   }
 
+  // If authenticated and scope access is granted, redirect to the dashboard page
   return (
-    <main>
-      <Outlet />
-    </main>
+    <Navigate
+      to={DASHBOARD_PATH}
+      replace
+    />
   );
 };
