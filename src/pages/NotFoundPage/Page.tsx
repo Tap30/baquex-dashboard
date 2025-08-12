8c612b1972283b47412649ccc1e8e3697e9da845
Amirhossein Alibakhshi <amir78729@gmail.com>
2025-08-12 11:30:41 +0330

    feat(page): add not found page

--- src/pages/NotFoundPage/Page.tsx
+++ src/pages/NotFoundPage/Page.tsx
@@ -1,7 +1,36 @@
+import { Exception, Icon } from "@/components";
+import { DASHBOARD_PATH, LOGIN_PATH } from "@/constants";
+import { useAuth } from "@/contexts";
+import { strings } from "@/static-content";
+import { mdiGoogleDownasaur } from "@mdi/js";
+import { useNavigate } from "react-router";
+
 export const NotFoundPage: React.FC = () => {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
+
+  const actionText = !isAuthenticated
+    ? strings.notFound.backToLogin
+    : strings.notFound.backToDashboard;
+
+  const actionFn = !isAuthenticated
+    ? () => void navigate(LOGIN_PATH)
+    : () => void navigate(DASHBOARD_PATH);
+
   return (
-    <main>
-      <h1>Not Found</h1>
-    </main>
+    <Exception
+      title={strings.notFound.title}
+      description={strings.notFound.description}
+      illustration={
+        <Icon
+          data={mdiGoogleDownasaur}
+          color="warn"
+        />
+      }
+      action={{
+        text: actionText,
+        onClick: actionFn,
+      }}
+    />
   );
 };
