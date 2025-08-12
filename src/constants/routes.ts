b4a1c83eb89b9b1514ba0b467d0b0d8bf3420aad
Amirhossein Alibakhshi <amir78729@gmail.com>
2025-08-12 11:30:14 +0330

    feat: add signing callback path

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,6 +2,7 @@ const BASE_AUTH_PATH = "/auth";
 
 export const LOGIN_PATH = `${BASE_AUTH_PATH}/login`;
 export const UNAUTHORIZED_PATH = `${BASE_AUTH_PATH}/access-denied`;
+export const SIGNIN_CALLBACK_PATH = `${BASE_AUTH_PATH}/signin-callback`;
 
 export const DASHBOARD_PATH = "/";
 export const SETTINGS_PATH = "/settings";
