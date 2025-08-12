34f918a96d0a29508122cd8deac6f234abdd47d8
Amirhossein Alibakhshi <amir78729@gmail.com>
2025-08-12 11:31:49 +0330

    feat(page): add access denied page

--- src/pages/AccessDeniedPage/Page.tsx
+++ src/pages/AccessDeniedPage/Page.tsx
@@ -1,3 +1,44 @@
+import { Exception, Icon } from "@/components";
+import { DASHBOARD_PATH, LOGIN_PATH } from "@/constants";
+import { useAuth } from "@/contexts";
+import { strings } from "@/static-content";
+import { mdiAccountCancel } from "@mdi/js";
+import { useNavigate } from "react-router";
+
 export const AccessDeniedPage: React.FC = () => {
-  return <section></section>;
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
+
+  const title = !isAuthenticated
+    ? strings.notAuthenticated.title
+    : strings.accessDenied.title;
+
+  const description = !isAuthenticated
+    ? strings.notAuthenticated.description
+    : strings.accessDenied.description;
+
+  const actionText = !isAuthenticated
+    ? strings.notAuthenticated.backToLogin
+    : strings.accessDenied.backToDashboard;
+
+  const actionFn = !isAuthenticated
+    ? () => void navigate(LOGIN_PATH)
+    : () => void navigate(DASHBOARD_PATH);
+
+  return (
+    <Exception
+      title={title}
+      description={description}
+      action={{
+        text: actionText,
+        onClick: actionFn,
+      }}
+      illustration={
+        <Icon
+          data={mdiAccountCancel}
+          color="negative"
+        />
+      }
+    />
+  );
 };
