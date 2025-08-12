b625bfe344596fe028859c7b806d674ea12d0434
Amirhossein Alibakhshi <amir78729@gmail.com>
2025-08-12 11:26:16 +0330

    feat(layout): add PageLoading component

--- src/components/layouts/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/layouts/ProtectedRoute/ProtectedRoute.tsx
@@ -1,3 +1,4 @@
+import { PageLoading } from "@/components";
 import { LOGIN_PATH, UNAUTHORIZED_PATH } from "@/constants";
 import { useAuth } from "@/contexts";
 import { Navigate, Outlet } from "react-router";
@@ -6,7 +7,7 @@ export const ProtectedRoute = () => {
   const { isAuthenticated, isAuthenticating, isAccessGranted } = useAuth();
 
   if (isAuthenticating) {
-    return <div>Authenticating...</div>;
+    return <PageLoading loading />;
   }
 
   // If not authenticated, redirect to the login page
