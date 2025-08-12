615d47c4a3ecc22e274a827dbd2da27d1533bde6
Amirhossein Alibakhshi <amir78729@gmail.com>
2025-08-12 12:11:29 +0330

    fix: resolve lint issue in dashboard page

--- src/pages/DashboardPage/Page.tsx
+++ src/pages/DashboardPage/Page.tsx
@@ -1,12 +1,8 @@
 import { strings } from "@/static-content";
 import { usePageBreadcrumb } from "@/utils";
-import { useLoaderData } from "react-router";
 import classes from "./styles.module.css";
-import { type PageLoaderData } from "./types.ts";
 
 export const DashboardPage: React.FC = () => {
-  const data = useLoaderData<PageLoaderData>();
-
   usePageBreadcrumb({
     title: strings.sidebar.dashboard,
     href: "/",
