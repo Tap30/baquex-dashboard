cb98f5b91fec022da9d412c59d2ca5ca6fb63d17
mimshins <mostafa.sh.coderino@gmail.com>
2025-08-13 14:49:57 +0330

    refactor(env): update to opt-in env global

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,6 @@
 export * from "./breakpoints.ts";
 export * from "./config.ts";
+export * from "./env.ts";
 export * from "./languages.ts";
 export * from "./routes.ts";
 export * from "./sidebar.ts";
