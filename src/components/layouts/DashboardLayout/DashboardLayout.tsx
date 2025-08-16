75b30a454cd1eb9d426040edf0f3bacb9644e27a
Amirhossein Alibakhshi <amir78729@gmail.com>
2025-08-16 11:35:03 +0330

    fix(dashboard layout): make sidebar expandable

--- src/components/layouts/DashboardLayout/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,22 +1,37 @@
-import { useState } from "react";
+import { cn, useMediaQuery } from "@/utils";
+import { useEffect, useRef, useState } from "react";
 import { Outlet } from "react-router";
+import { ClickableArea } from "../../blocks/index.ts";
 import { BreadcrumbProvider } from "./BreadcrumbContext/index.ts";
 import { Header, Sidebar } from "./components/index.ts";
 import classes from "./styles.module.css";
 
 export const DashboardLayout: React.FC = () => {
+  const [isMobile] = useMediaQuery("(max-width: 786px)");
   const [isNavOpen, setIsNavOpen] = useState(true);
+
+  const rootRef = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    setIsNavOpen(!isMobile);
+  }, [isMobile]);
 
   const toggleNav = () => {
     setIsNavOpen(o => !o);
   };
 
+  const closeNav = () => {
+    setIsNavOpen(false);
+  };
+
   return (
-    <main className={classes["root"]}>
-      <Sidebar
-        className={classes["sidebar"]}
-        open={isNavOpen}
-      />
+    <main
+      ref={rootRef}
+      className={cn(classes["root"], {
+        [classes["nav-open"]!]: isNavOpen,
+        [classes["mobile"]!]: isMobile,
+      })}
+    >
       <section className={classes["content"]}>
         <BreadcrumbProvider>
           <Header
@@ -27,6 +42,15 @@ export const DashboardLayout: React.FC = () => {
           <Outlet />
         </BreadcrumbProvider>
       </section>
+      {isMobile && isNavOpen && (
+        <ClickableArea onClick={closeNav}>
+          <div className={classes["overlay"]}></div>
+        </ClickableArea>
+      )}
+      <Sidebar
+        className={classes["sidebar"]}
+        open={isNavOpen}
+      />
     </main>
   );
 };
