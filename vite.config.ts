abb92fd1b8664ff1779aaa87608652710ad1d49f
Mostafa Shamsitabar <mostafa.sh.coderino@gmail.com>
2025-11-18 12:23:08 +0330

    update chunking

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,7 @@ const config = defineConfig(({ mode }) => {
   const env = loadEnv(mode, import.meta.dirname) as unknown as AppEnv;
 
   const domain = env["VITE_APP_HOSTNAME"] || "https://e8t.tapsi.tech";
-
-  // Remove any leading protocol (e.g., http://, https://)
-  const certHost = domain.replace(/^(https?:\/\/)/, "");
+  const host = new URL(domain);
 
   const baseConfig = {
     plugins: [tailwindPlugin(), tsconfigPathsPlugin(), reactPlugin()],
@@ -21,15 +19,43 @@ const config = defineConfig(({ mode }) => {
     ...baseConfig,
     plugins: [
       ...baseConfig.plugins,
-      mkcert({ savePath: ".certs", hosts: [certHost] }),
+      mkcert({ savePath: ".certs", hosts: [host.hostname] }),
     ],
     server: {
       port: 443,
-      open: domain,
+      open: host.origin,
     },
   };
 
-  return mode === "development" ? devConfig : baseConfig;
+  const prodConfig: UserConfig = {
+    ...baseConfig,
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              const modulePath = id.split("node_modules/")[1];
+              const topLevelFolder = modulePath?.split("/")[0];
+
+              if (topLevelFolder !== ".pnpm") return topLevelFolder;
+
+              const scopedPackageName = modulePath?.split("/")[1];
+              const chunkName =
+                scopedPackageName?.split("@")[
+                  scopedPackageName.startsWith("@") ? 1 : 0
+                ];
+
+              return chunkName;
+            }
+
+            return undefined;
+          },
+        },
+      },
+    },
+  };
+
+  return mode === "development" ? devConfig : prodConfig;
 });
 
 export default config;
